a535becdf28af07cb70cec8af1fa2776101d998c
ZGHAIA@gmail.com <ZGHAIA@gmail.com@a2033596-31a6-069e-24f2-965f752cb14c>
2011-09-21 10:28:43 +0000

    version 1.4 go on for developing ,but the shut down service can't run .

--- trunk/src/zghaia/android/drcom/Drcom.java
+++ trunk/src/zghaia/android/drcom/Drcom.java
@@ -1,23 +1,18 @@
+/*
+  Copyright (C) <2011> <Dr.com for Android Authors :zghaia@gmail.com>
+                          All rights reserved.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+               GNU General Public License for more details 
+                      <./GNU GENERAL PUBLIC LICENSE>
+ */
+
 package zghaia.android.drcom;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.app.Notification;
@@ -33,14 +28,16 @@ import android.widget.Toast;
 
 public class Drcom extends Activity {
 	private Button bLogin,bLogout,bSetting,bAbout;
-
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        //init Button must before use it
+        final Http http=new Http();
+        
+        //init Button must before using it
         bLogin=(Button)findViewById(R.id.login);
         bLogout=(Button)findViewById(R.id.logout);
         bSetting=(Button)findViewById(R.id.setting);
@@ -82,7 +79,11 @@ public class Drcom extends Activity {
         	@Override
         	public void onClick(View v){
         		String server="http://"+address+"/";
-        		login(server,user,password);	
+        		if(http.login(server,user,password)){
+        			showToast(R.string.login_success);	
+        		}else{
+        			showToast(R.string.login_fail);
+        		}
         		eDrcom.putInt("state", 1);
         		eDrcom.commit();
         		bLogin.setEnabled(false);
@@ -95,7 +96,11 @@ public class Drcom extends Activity {
         	@Override
         	public void onClick(View v){
         		String server="http://"+address+"/F.htm";
-        		logout(server);
+        		if(http.logout(server)){
+        			showToast(R.string.logout_success);	
+        		}else{
+        			showToast(R.string.logout_fail);
+        		}
         		eDrcom.putInt("state", 0);
         		eDrcom.commit();
         		bLogin.setEnabled(true);
@@ -140,146 +145,10 @@ public class Drcom extends Activity {
     	nDrcom.setLatestEventInfo(this, "Dr.com", "Dr.com", intent);
     	mDrcom.notify( iDrcom, nDrcom); 
     }
-    
+	
     //showToast
     public void showToast(int toastId){
     	Toast.makeText( this,getString(toastId),Toast.LENGTH_LONG).show();
     }
-    
-    // login function
-    public void login(String server,String user,String password){
-    	BufferedReader in = null;
-		try {
-			HttpClient client = new DefaultHttpClient();
-			HttpPost request = new HttpPost(server);
-			List<NameValuePair> postParameters = new ArrayList<NameValuePair>();
-			postParameters.add(new BasicNameValuePair("DDDDD", user));
-			postParameters.add(new BasicNameValuePair("upass", password));
-			postParameters.add(new BasicNameValuePair("0MKKey", "%B5%C7%C2%BC+Login"));
-			UrlEncodedFormEntity formEntity = null;
-			try {
-				formEntity = new UrlEncodedFormEntity(postParameters);
-			} catch (UnsupportedEncodingException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			request.setEntity(formEntity);
-			HttpResponse response = null;
-			try {
-				response = client.execute(request);
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			StringBuffer sb = new StringBuffer("");
-			String line = "";
-			String NL = System.getProperty("line.separator");
-			try {
-				while ((line = in.readLine()) != null) {
-					sb.append(line + NL);
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				in.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			String page = sb.toString();
-			System.out.println(page);
-			showToast(R.string.login_success);
-			} finally {
-				if (in != null) {
-					try {
-						in.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-						showToast(R.string.login_fail);
-					}
-				}
-				
-			}
-    }
-    
-    //logout function
-    public void logout(String server){
-    	BufferedReader in=null;
-		try {//HttpGet
-			HttpClient client = new DefaultHttpClient();
-			HttpGet request = new HttpGet();
-			try {
-				request.setURI(new URI(server));
-			} catch (URISyntaxException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			HttpResponse response = null;
-			try {
-				response = client.execute(request);
-			} catch (ClientProtocolException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			//get Response  Content
-			try {
-				in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
-			} catch (IllegalStateException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			StringBuffer sb = new StringBuffer("");
-			String line = "";
-			String NL = System.getProperty("line.separator");
-			//print the Response  Content
-			try {
-				while ((line = in.readLine()) != null) {
-					sb.append(line + NL);
-				}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				in.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			String page = sb.toString();
-			System.out.println(page);
-			showToast(R.string.logout_success);
-			//error control
-		} finally {
-			if (in != null) {
-				try {
-					in.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-					showToast(R.string.logout_fail);
-				}
-			}
-			
-		}
-    }
+
 }
